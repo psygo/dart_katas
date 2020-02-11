13df283e3521f0de32514b79d62efe23256f9f5d
Philippe Fanaro <philippefanaro@gmail.com>
2020-02-11 14:20:15 -0300

    first skeleton of the GameOfLife class

--- lib/game_of_life.dart
+++ lib/game_of_life.dart
@@ -1,3 +1,37 @@
+/*
+# Game of Life's Rules
+
+Any live cell with:
+
+1. fewer than two live neighbours dies, as if caused by underpopulation.
+2. more than three live neighbours dies, as if by overcrowding.
+3. two or three live neighbours lives on to the next generation.
+4. exactly three live neighbours becomes a live cell.
+*/
+
+import 'package:flutter/foundation.dart';
+
+
 class GameOfLife {
-  
+
+  final int height;
+  final int width;
+  List<List<String>> _grid;
+
+  GameOfLife({
+    @required this.height,
+    @required this.width,
+  }){
+    _grid = _emptyGrid(height: height, width: width);
+  }
+
+  get grid => _grid;
+
+  List<List<String>> _emptyGrid({
+    int height,
+    int width,
+  }){
+    List<String> emptyLine = List<String>.filled(width, '-', growable: false);
+    return List<List<String>>.filled(height, emptyLine, growable: false);
+  }
 }
--- test/game_of_life_test.dart
+++ test/game_of_life_test.dart
@@ -5,8 +5,24 @@ import '../lib/game_of_life.dart';
 
 void main(){
 
-  test('', (){
-    
+  GameOfLife _game;
+
+  setUp((){
+    _game = GameOfLife(
+      height: 4,
+      width: 8,
+    );
+  });
+
+  test('Empty Grid', (){
+    List<List<String>> emptyGrid = [
+      ['-', '-', '-', '-', '-', '-', '-', '-'],
+      ['-', '-', '-', '-', '-', '-', '-', '-'],
+      ['-', '-', '-', '-', '-', '-', '-', '-'],
+      ['-', '-', '-', '-', '-', '-', '-', '-'],
+    ];
+
+    expect(_game.grid, emptyGrid);
   });
 
 }
