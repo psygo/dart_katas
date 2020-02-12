3f100543dd84e86da9232e0d1d9a270d7a28d69e
Philippe Fanaro <philippefanaro@gmail.com>
2020-02-12 16:53:07 -0300

    first implementation of the core rules

--- lib/game_of_life/game_of_life.dart
+++ lib/game_of_life/game_of_life.dart
@@ -79,42 +79,61 @@ class GameOfLife {
 
     _gridParser
       .heightWidthLooper(_height, _width, (int heightIndex, int widthIndex){
+        
+        Cell currentCell = baseGrid[heightIndex][widthIndex];
+        int totalAliveNeighbors = 0;
+        
         _vicinityLooper(heightIndex, widthIndex, 
-          (int heightStep, int widthStep){
+          (int neighborHeightIndex, int neighborWidthIndex){
             try {
-
+              Cell neighborCell 
+                = baseGrid[neighborHeightIndex][neighborWidthIndex];
+              if (neighborCell.isAlive){
+                totalAliveNeighbors++;
+              }
             } catch(e) {
 
             }
           });
+        
+        if (_willLive(currentCell.isAlive, totalAliveNeighbors)){
+          baseGrid[heightIndex][widthIndex].status = Status.alive;
+        }
       });
   }
 
   _vicinityLooper(
     int heightIndex,
     int widthIndex,
-    Function(int heightStep, int widthStep) function,
+    Function(int neighborHeightIndex, int neighborWidthIndex) function,
   ){
     for (int heightStep = -1; heightStep < 1; heightStep++){
       for (int widthStep = -1; widthStep < 1; widthStep++){
-        if (_isNotCellItself(heightStep, widthStep)){
+        if (_isNotTheCellItself(heightStep, widthStep)){
           int neighborHeightIndex = heightIndex + heightStep;
           int neighborWidthIndex = widthIndex + widthStep;
-          function(heightStep, widthStep);
+          function(neighborHeightIndex, neighborWidthIndex);
         }
       }
     }
   }
 
-  bool _isNotCellItself(heightStep, widthStep) => 
+  bool _isNotTheCellItself(heightStep, widthStep) => 
     !(heightStep == 0 && widthStep == 0);
 
+  bool _willLive(
+    bool isAlive,
+    int totalAliveNeighbors,
+  ){
+    if (isAlive && totalAliveNeighbors < 2){
+      return false;
+    } else if (isAlive && totalAliveNeighbors > 3){
+      return false;
+    } else if (isAlive && 
+      (totalAliveNeighbors == 2 || totalAliveNeighbors == 3)){
+      return true;
+    } else if (!isAlive && totalAliveNeighbors == 3){
+      return true;
+    }
+  }
 }
-
-
-
-
-
-
-
-
