006968ca104025bd43dbdeb2bf46b5f25502a9be
Philippe Fanaro <philippefanaro@gmail.com>
2020-02-13 14:15:34 -0300

    fixed imprecisions in the declaration of rules at the top comment

--- lib/game_of_life/game_of_life.dart
+++ lib/game_of_life/game_of_life.dart
@@ -3,10 +3,13 @@
 
 Any live cell with:
 
-1. fewer than two live neighbours dies, as if caused by underpopulation.
-2. more than three live neighbours dies, as if by overcrowding.
-3. two or three live neighbours lives on to the next generation.
-4. exactly three live neighbours becomes a live cell.
+  1. fewer than two live neighbours dies, as if caused by underpopulation.
+  2. more than three live neighbours dies, as if by overcrowding.
+  3. two or three live neighbours lives on to the next generation.
+
+Any dead cell with:
+
+  4. exactly three live neighbours becomes a live cell.
 */
 
 import 'package:collection/collection.dart';
