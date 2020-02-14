6d67a01d459ee2cbf995d00bb6d1105f373a971a
Philippe Fanaro <philippefanaro@gmail.com>
2020-02-14 18:42:24 -0300

    refactored the tests of prime factoring into one single test (!!!)

--- test/tests/prime_factors_test.dart
+++ test/tests/prime_factors_test.dart
@@ -1,36 +1,17 @@
 import 'package:test/test.dart';
 
 import '../../lib/prime_factors/prime_factors.dart';
+import '../mocks/prime_factors_mocks.dart';
 
 
 void main() {
 
-  test('testFactoringOne', (){
-    expect(PrimeFactors.generate(1), []);
-  });
-
-  test('testFactoringTwo', (){
-    expect(PrimeFactors.generate(2), [2]);
-  });
-
-  test('testFactoringThree', (){
-    expect(PrimeFactors.generate(3), [3]);
-  });
-
-  test('testFactoringFour', (){
-    expect(PrimeFactors.generate(4), [2, 2]);
-  });
-
-  test('testFactoringSix', (){
-    expect(PrimeFactors.generate(6), [2, 3]);
-  });
-
-  test('testFactoringEight', (){
-    expect(PrimeFactors.generate(8), [2, 2, 2]);
-  });
-
-  test('testFactoringNine', (){
-    expect(PrimeFactors.generate(9), [3, 3]);
+  test('Many Basic Prime factorings', (){
+    numberAndPrimeFactors.forEach(
+      (int number, List<int> primeFactors){
+        expect(PrimeFactors.generate(number), primeFactors);
+      }
+    );
   });
   
 }
