00c70dfc4d2482d53e674bc1f1d272dc8495d1a8
Philippe Fanaro <philippefanaro@gmail.com>
2020-02-14 12:52:36 -0300

    added an if to only loop through bases that haven't been excluded | inverted the semantics of the booleanExcluder

--- lib/eratos_sieve/eratos_sieve.dart
+++ lib/eratos_sieve/eratos_sieve.dart
@@ -33,14 +33,17 @@ class EratosSievePrimeGenerator implements PrimeGenerator {
       _generateBooleanExcluders(_upperInclusiveLimit);
     final int topRange = _calculateTopRange(_upperInclusiveLimit);
 
-    for (int baseOfSieve = 2; baseOfSieve <= topRange; baseOfSieve++){
-      for (
-        int testedInteger = pow(baseOfSieve, 2).toInt(); 
-        testedInteger <= _upperInclusiveLimit; 
-        testedInteger += baseOfSieve
-      ){
-        final int testedIntegerIndex = allIntegers.indexOf(testedInteger);
-        booleanExcluders[testedIntegerIndex] = false;
+    for (int baseOfSieve = 2; baseOfSieve < topRange; baseOfSieve++){
+      final int baseOfSieveIndex = allIntegers.indexOf(baseOfSieve);
+      if (_isBaseNotExcluded(booleanExcluders, baseOfSieveIndex)){
+        for (
+          int testedInteger = pow(baseOfSieve, 2).toInt(); 
+          testedInteger <= _upperInclusiveLimit; 
+          testedInteger += baseOfSieve
+        ){
+          final int testedIntegerIndex = allIntegers.indexOf(testedInteger);
+          booleanExcluders[testedIntegerIndex] = true;
+        }
       }
     }
 
@@ -51,18 +54,21 @@ class EratosSievePrimeGenerator implements PrimeGenerator {
     => List<int>.generate(upperInclusiveLimit, (int i) => i + 2);
 
   List<bool> _generateBooleanExcluders(int upperInclusiveLimit)
-    => List<bool>.filled(upperInclusiveLimit - 1, true);
+    => List<bool>.filled(upperInclusiveLimit - 1, false);
 
   int _calculateTopRange(int upperInclusiveLimit) 
-    => sqrt(upperInclusiveLimit).ceil() + 1;  
+    => sqrt(upperInclusiveLimit).ceil() + 1;
+
+  bool _isBaseNotExcluded(List<bool> booleanExcluders, int baseIndex) 
+    => !booleanExcluders[baseIndex];
 
   List<int> _filterIntegers(
     List<int> allIntegers,
-    List<bool> booleanFilter,
+    List<bool> booleanExcluderFilter,
   ) => allIntegers.where(
     (int integer){
       final int integerIndex = allIntegers.indexOf(integer);
-      return booleanFilter[integerIndex]; 
+      return !booleanExcluderFilter[integerIndex]; 
     }
   ).toList();
   
