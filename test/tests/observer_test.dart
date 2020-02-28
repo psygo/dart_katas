aedb489339fb2387b1ddb0ee80106cd989141be5
Philippe Fanaro <philippefanaro@gmail.com>
2020-02-27 22:29:39 -0300

    adding dartfmt

--- test/tests/observer_test.dart
+++ test/tests/observer_test.dart
@@ -2,23 +2,19 @@ import 'package:test/test.dart';
 
 import '../../lib/design_patterns/observer/observer.dart';
 
-
-void main(){
-
-  group('Test Counter with an Observable property', (){
-    test('Synchronous appending from listening to the stream', (){
+void main() {
+  group('Test Counter with an Observable property', () {
+    test('Synchronous appending from listening to the stream', () {
       final ObservableCounter observableCounter = ObservableCounter();
       final List<int> intsFromStream = [];
 
-      observableCounter.listen(
-        (ObservableCounter counterInstance) {
-          intsFromStream.add(counterInstance.count);
-        }
-      );
-      
+      observableCounter.listen((ObservableCounter counterInstance) {
+        intsFromStream.add(counterInstance.count);
+      });
+
       const int totalLoops = 10;
-      final List<int> matcherList 
-        = List<int>.generate(totalLoops, (int index) => index + 1);
+      final List<int> matcherList =
+          List<int>.generate(totalLoops, (int index) => index + 1);
       for (int loopIndex = 0; loopIndex < totalLoops; loopIndex++) {
         observableCounter.increment();
       }
@@ -28,5 +24,4 @@ void main(){
       expect(intsFromStream, matcherList);
     });
   });
-
 }
