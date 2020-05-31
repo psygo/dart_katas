6ae0f63dea2b9fab106483ff93dbf69aca453ae7
Philippe Fanaro <philippefanaro@gmail.com>
2020-05-31 10:24:30 -0300

    trying to get a population working

--- lib/genetic_algorithm/genetic_algorithm.dart
+++ lib/genetic_algorithm/genetic_algorithm.dart
@@ -9,17 +9,65 @@ abstract class EvolutionSimulator {
   void evolve({int cycles = 1});
 }
 
+@immutable
 abstract class Population {
-  Iterable<Individual> individuals;
+  Iterable<Individual> get individuals;
+
+  factory Population.getRandomPopulation({
+    size = 100,
+    individualLength,
+    randomGeneratorCeiling,
+  }) =>
+      RandomPopulation(
+        size: size,
+        individualLength: individualLength,
+        randomGeneratorCeiling: randomGeneratorCeiling,
+      );
+
+  Population();
+}
+
+@immutable
+class RandomPopulation implements Population {
+  final List<Individual> _individuals;
+
+  RandomPopulation({
+    size,
+    individualLength,
+    randomGeneratorCeiling,
+  }) : _individuals = _createRandomIndividualsList(
+          size,
+          individualLength,
+          randomGeneratorCeiling,
+        );
+
+  static List<Individual> _createRandomIndividualsList(
+    int size,
+    int individualLength,
+    int randomGeneratorCeiling,
+  ) =>
+      List.generate(
+          size,
+          (int _) => Individual.getRandomIndividual(
+              length: individualLength,
+              randomGeneratorCeiling: randomGeneratorCeiling));
+
+  List<Individual> get individuals => _individuals;
+}
+
+mixin PopulationEquality on Population {
+  @override
+  bool operator ==(Object otherObject) =>
+      otherObject is Population && otherObject.individuals == individuals;
 }
 
 @immutable
 abstract class Individual {
-  factory Individual.getBasicIndividual({
+  factory Individual.getRandomIndividual({
     length = 1,
     randomGeneratorCeiling = 1000,
   }) =>
-      IndividualBasicImpl(
+      RandomIndividual(
         length: length,
         randomGeneratorCeiling: randomGeneratorCeiling,
       );
@@ -30,16 +78,21 @@ abstract class Individual {
 }
 
 @immutable
-class IndividualBasicImpl implements Individual {
+class RandomIndividual implements Individual {
   static final Random randomNumberGenerator = Random();
 
   final List<int> _values;
 
-  IndividualBasicImpl({
+  RandomIndividual({
     length,
     randomGeneratorCeiling,
-  }) : _values = List.generate(length,
-            (int _) => randomNumberGenerator.nextInt(randomGeneratorCeiling));
+  }) : _values = _createRandomList(length, randomGeneratorCeiling);
+
+  static List<int> _createRandomList(
+    int length,
+    int ceiling,
+  ) =>
+      List.generate(length, (int _) => randomNumberGenerator.nextInt(ceiling));
 
   List<int> get values => _values;
 }
--- test/tests/genetic_algorithm_test.dart
+++ test/tests/genetic_algorithm_test.dart
@@ -3,9 +3,9 @@ import 'package:test/test.dart';
 
 void main() {
   group('`Individual`', () {
-    test('Checks that 2 individuals don\'t have the same parameters', () {
-      final Individual individual1 = Individual.getBasicIndividual(length: 10);
-      final Individual individual2 = Individual.getBasicIndividual(length: 10);
+    test('Checks that 2 random individuals don\'t have the same parameters', () {
+      final Individual individual1 = Individual.getRandomIndividual(length: 10);
+      final Individual individual2 = Individual.getRandomIndividual(length: 10);
 
       expect(individual1, equals(individual1));
       expect(individual1 == individual2, isFalse);
@@ -13,8 +13,11 @@ void main() {
   });
 
   group('Population', () {
-    test('', () {
-      
+    test('Checks that 2 random populations don\'t have the same individuals', () {
+      final Population population1 = Population.getRandomPopulation();
+      final Population population2 = Population.getRandomPopulation();
+
+      expect(population1, equals(population1));
     });
   });
 }
