daa46eff5ffb2d72acea82d38d617633502ad3de
Philippe Fanaro <philippefanaro@gmail.com>
2020-05-31 22:41:00 -0300

    changed the structure of the game of life

---
+++ game_of_life/lib/game_of_life.dart
@@ -0,0 +1,5 @@
+library game_of_life;
+
+export 'src/cell.dart' show Cell, Status;
+export 'src/game_of_life.dart' show GameOfLife;
+export 'src/grid_parser.dart' show GridParser;
---
+++ game_of_life/lib/src/cell.dart
@@ -0,0 +1,32 @@
+enum Status {
+  dead,
+  alive,
+}
+
+class Cell {
+  Status _status;
+
+  Cell(
+    Status status,
+  ) : _status = status;
+
+  factory Cell.dead() => Cell(Status.dead);
+  factory Cell.alive() => Cell(Status.alive);
+
+  Status get status => _status;
+  bool get isAlive => _status == Status.alive;
+  bool get isDead => _status == Status.dead;
+
+  void set status(
+    Status newStatus,
+  ) {
+    _status = newStatus;
+  }
+
+  @override
+  bool operator ==(otherObject) =>
+      otherObject is Cell && otherObject.status == _status;
+
+  @override
+  int get hashCode => _status.hashCode;
+}
