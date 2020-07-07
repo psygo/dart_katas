7fa5ae5ecdea1b038e2958e51b8ff97a5adbfe3e
Philippe Fanaro <Philippe>
2020-07-07 17:52:10 -0300

    boilerplate for the erlang kata

---
+++ katas/erlang/lib/erlang.dart
@@ -0,0 +1,6 @@
+/// Kata of Erlang's B and C functions, based on an 
+/// [old post of mine](https://fanaro.com.br/telecom-nostalgia-and-python-basics/)
+/// {@category Telecommunications}
+library erlang;
+
+export 'src/erlang.dart';
