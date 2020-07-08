66b185229420ca54f9e58c740045ad4de700c6d8
Philippe Fanaro <Philippe>
2020-07-08 16:35:13 -0300

    boilerplate for the erlang calculator

--- katas/erlang/lib/src/erlang.dart
+++ katas/erlang/lib/src/erlang.dart
@@ -1,1 +1,27 @@
+import 'package:meta/meta.dart';
 
+@immutable
+class Erlang {
+  final double _callRate, _callTime;
+
+  const Erlang({
+    @required double callRate,
+    @required double callTime,
+  })  : _callRate = callRate,
+        _callTime = callTime;
+
+  double get e => _callRate * _callTime;
+}
+
+@immutable
+class ErlangCalculator {
+  const ErlangCalculator();
+
+  double calcB(double e, int n) {
+    return 0;
+  }
+
+  double caclC(double e, int n) {
+    return 0;
+  }
+}
