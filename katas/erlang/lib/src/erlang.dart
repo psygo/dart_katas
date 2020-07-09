38abaecbde77e8796a419b3b15b637de55438c46
Philippe Fanaro <Philippe>
2020-07-08 21:59:14 -0300

    slightly safer B function

--- katas/erlang/lib/src/erlang.dart
+++ katas/erlang/lib/src/erlang.dart
@@ -28,13 +28,18 @@ class ErlangCalculator {
 
   /// Probability of blockage with Erlang's B function.
   double calcB() {
-    final double top = pow(_erlangs.e, _numChannels) / _factorial(_numChannels);
-    final List<double> bottomVector = List<double>.generate(
-        _numChannels + 1,
-        (int channelIndex) =>
-            pow(_erlangs.e, channelIndex) / _factorial(channelIndex));
-    final double bottom = bottomVector.reduce((double a, double b) => a + b);
-    return top / bottom;
+    if (_numChannels == 0) {
+      return 1;
+    } else {
+      final double top =
+          pow(_erlangs.e, _numChannels) / _factorial(_numChannels);
+      final List<double> bottomVector = List<double>.generate(
+          _numChannels + 1,
+          (int channelIndex) =>
+              pow(_erlangs.e, channelIndex) / _factorial(channelIndex));
+      final double bottom = bottomVector.reduce((double a, double b) => a + b);
+      return top / bottom;
+    }
   }
 
   int _factorial(int n) => n == 0 ? 1 : n * _factorial(n - 1);
